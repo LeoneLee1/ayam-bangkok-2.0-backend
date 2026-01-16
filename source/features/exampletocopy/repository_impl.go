a35a8824bf810284d8045c3d9ddd9dbe3c6fcfed
Daniel <danielruntuwene42@gmail.com>
2026-01-16 15:01:18 +0700

    Initial commit

--- source/features/exampletocopy/repository_impl.go
+++
@@ -1,8 +0,0 @@
-package exampletocopy
-
-import "context"
-
-// LogUserAccess implements Repository.
-func (r *repositoryImpl) LogUserAccess(ctx context.Context, userID uint, requesterIP string) error {
-	panic("unimplemented")
-}
--- source/features/exampletocopy/usecase_impl.go
+++
@@ -1,8 +0,0 @@
-package exampletocopy
-
-import "context"
-
-// LogUserAccess implements Usecase.
-func (u *usecaseImpl) LogUserAccess(ctx context.Context, userID uint, requesterIP string) error {
-	panic("unimplemented")
-}

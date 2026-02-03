f07b704807fcd6dd215d7028666d45393952b34b
Daniel <danielruntuwene42@gmail.com>
2026-02-03 21:34:06 +0700

    fixing bug feature get menu filter by time

--- source/features/menu/menu_get/usecase_impl.go
+++ source/features/menu/menu_get/usecase_impl.go
@@ -3,7 +3,6 @@ package menuget
 import (
 	"ayam_bangkok/source/common/models"
 	"context"
-	"time"
 )
 
 // getMenu implements Usecase.
@@ -11,18 +10,6 @@ func (u *usecaseImpl) getMenu(ctx context.Context, day *string, week *int, page,
 	offset := (page - 1) * limit
 	if offset < 0 {
 		offset = 0
-	}
-
-	now := time.Now()
-
-	if day == nil {
-		d := now.Weekday().String()
-		day = &d
-	}
-
-	if week == nil {
-		_, w := now.ISOWeek()
-		week = &w
 	}
 
 	return u.repo.getMenu(ctx, day, week, page, limit, offset)
